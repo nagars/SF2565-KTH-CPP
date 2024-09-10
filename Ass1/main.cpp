663e78ede12526e216840f9d13ccc5f4bc412f27
nagars <shawnnagar@gmail.com>
2024-09-10 13:59:26 +0200

    Init commit assignment 1

---
+++ Ass1/main.cpp
@@ -0,0 +1,43 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Sep 9, 2024
+ *      Author: root
+ */
+
+#include <cmath>
+
+namespace std;
+
+// Function to test
+double func (double x){
+	return abs(x + cos(pow(x,5)));
+}
+///////////////////////
+
+////Simpsons functions////
+
+double func_integration(double(*f)(double), double begin_limit, double end_limit, double tolerance){
+
+	// Initialisations
+	double x = 0;			// fed to function to be tested
+	double integral_1 = 0;	// I1 integration
+	double integral_2 = 0;	// I2 integration
+
+	// Check if begin_limit < end_limit, else return with -1
+
+	//
+
+}
+
+//////////////////////////
+
+
+int main(){
+
+	// Call recursive function
+
+	return 1;
+}
+
+
