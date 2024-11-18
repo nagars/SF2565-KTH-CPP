86d9f5f924742a9e7f5fcf4ee9795ff1af4e1d10
sloth49 <158371384+sloth49@users.noreply.github.com>
2024-11-18 23:13:18 +0100

    added structure of BottomCurve and check it compiles

--- Assign3/class_def.hpp
+++ Assign3/class_def.hpp
@@ -8,7 +8,7 @@
 #ifndef CLASS_DEF_HPP_
 #define CLASS_DEF_HPP_
 
-#include <math.h>
+#include <cmath>
 #include <vector>
 #include <memory>
 #include <cassert>
@@ -35,7 +35,7 @@ protected:
 class Curve {
 public:
 	virtual ~Curve() = default;
-	virtual Point at(double t) const = 0;
+	virtual Point at_t(double t) const = 0;
 private:
 protected:
 };
@@ -48,7 +48,7 @@ public:
 		pointStart(a), pointEnd(b){}
 
 	// t = {0,1}. t = 0 -> pointStart. t = 1 -> pointEnd
-	Point at(double t) const override {
+	Point at_t(double t) const override {
 		// Ensure points are not the same
 		assert(fabs(pointEnd.x - pointStart.x) < EPSILON);
 		assert(fabs(pointEnd.y - pointStart.y) < EPSILON);
@@ -68,7 +68,8 @@ private:
 class EquationCurve : public Curve {	
 	public:
 		virtual ~EquationCurve() = default; 
-		Point at(double t) const override;
+		Point at_t(double t) const override;
+		virtual Point at_hatS(double hatS) const = 0;
 	private:
 		virtual Point gamma(double t) const = 0; 
 		virtual Point gammaprime(double t) const = 0;
@@ -79,10 +80,61 @@ class BottomCurve : public EquationCurve {
 	public:
 		// constructor (empty)
 		// destructor (is it needed?)
-		// 'at' method
+		// 'at' methods
+		Point at_t(double t) {
+			return gamma(t);
+		}
+		Point at_hatS(double hatS) {
+			// compute t* = t(hatS;s(1)), using Newton's method in lecture 8 slide 31,
+			//                            which uses gammaprime
+			// get gamma(t*)
+		} 
 	private:
-		// gamma hardcode the f(x) in the assignment with if/else for branching
-		// gammaprime - do it analitically first
+		// domain of bottom curve in cartesian parametrisation
+		static constexpr double X_MIN = -10;
+		static constexpr double X_MID = -3;
+		static constexpr double X_MAX = 5;
+		static constexpr double X_DELTA = X_MAX - X_MIN;
+
+		// TODO compute s(1)
+			// use ASI
+			// int_0^1 gammaprime(t) dt
+
+		// Helper functions for gamma and gammaprime methods
+		// Refer to assignment page 1 for symbols
+		double x(double t) const {
+			return X_MIN + X_DELTA*t; 
+		}
+		double g(double x) const {
+			if (x < X_MID) {
+				return 1 + std::exp(-3*(x+6)) ;
+			} else {
+				return 1 + std::exp(3*x);
+			}
+		}
+		// returns P(t)=(x(t), y(t))
+		Point gamma(double t) {
+			// TODO add check for out-of-bounds argument
+			double x_of_t = x(t);
+			double g_of_x = g(x_of_t);
+			// f{g[x(t)]}
+			double f_of_x = 1/(2*g_of_x);
+			return Point(x_of_t, f_of_x);
+		}
+		// returns P_dot(t)=(x_dot(t), y_dot(t))
+		Point gammaprime(double t) {
+			double x_of_t = x(t);
+			double g_of_x = g(x_of_t);
+			double x_dot = X_DELTA;
+			double g_prime;
+			if (x_of_t < X_MID) {
+				g_prime = 1 + std::exp(-3*(x_of_t+6));
+			} else {
+				g_prime = 1 + std::exp(3*x_of_t);
+			}
+			double y_dot = x_of_t / (-2*g_of_x) * g_prime;
+			return Point(x_dot, y_dot);
+		}
 };
 
 // Used to represent the grid
