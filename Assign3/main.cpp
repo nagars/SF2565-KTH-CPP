1fe42f9eb56ce932776972770d961d3e969a449d
sloth49 <158371384+sloth49@users.noreply.github.com>
2024-11-21 13:30:09 +0100

    modified structure of classes to include a grid object and implement TFI. Compiles, not tested.

--- Assign3/main.cpp
+++ Assign3/main.cpp
@@ -5,11 +5,12 @@
  *      Author: Shawn
  */
 #include "class_def.hpp"
+#include <iostream>
 
 // n > 2
-#define NUM_DIVISIONS	5
+#define NUM_DIVISIONS 5
 // h = 1/n
-#define GRID_POINT_SPACING	1/NUM_DIVISIONS
+// #define GRID_POINT_SPACING	1/NUM_DIVISIONS
 
 // (y,x)  coordinates
 #define TOPLEFT 0,5
@@ -21,11 +22,28 @@
 
 int main(){
 
+	// Point topLeft(TOPLEFT);
+	// Point topRight(TOPRIGHT);
+	// Point bottomLeft(BOTTOMLEFT);
+	// Point bottomRight(BOTTOMRIGHT);
+	// Domain grid(bottomLeft, bottomRight, topLeft, topRight);
 	Point topLeft(TOPLEFT);
 	Point topRight(TOPRIGHT);
 	Point bottomLeft(BOTTOMLEFT);
 	Point bottomRight(BOTTOMRIGHT);
-	Domain grid(bottomLeft, bottomRight, topLeft, topRight);
+	std::unique_ptr<Curve> bottom = std::make_unique<StraightLine>(bottomLeft, bottomRight);
+	std::unique_ptr<Curve> top = std::make_unique<StraightLine>(topLeft, topRight);
+	std::unique_ptr<Curve> left = std::make_unique<StraightLine>(bottomLeft, topLeft);
+	std::unique_ptr<Curve> right = std::make_unique<StraightLine>(bottomRight, topRight);
 
-	return 1;
+	Domain linearDomain(std::move(bottom), std::move(top),
+						std::move(left), std::move(right),
+						NUM_DIVISIONS);
+	linearDomain.GenerateGrid();
+	
+	const auto& grid = linearDomain.GetGrid();
+    std::cout << "Grid X coordinates:\n" << grid.GetX() << "\n";
+    std::cout << "Grid Y coordinates:\n" << grid.GetY() << "\n";
+
+	return 0;
 }
