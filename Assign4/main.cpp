ae569ea06864d87f38dc77197af0d32a25c886aa
sloth49 <158371384+sloth49@users.noreply.github.com>
2024-12-08 23:25:22 +0100

    Added function to generate initial conditions. Started function to calculate cumulative prob. of extinction times

--- Assign4/main.cpp
+++ Assign4/main.cpp
@@ -5,17 +5,60 @@ Created on: Dec 6, 2024
 */
 
 #include <random>
-
 #define dt 1e-3  // time step
 
 
-void dGamma(double b) {
-    // Gamma probability distribution
-    // C++ ref: https://en.cppreference.com/w/cpp/numeric/random/gamma_distribution
-    // theory:  https://en.wikipedia.org/wiki/Gamma_distribution
+// Generate M initial conditions for equation (1) in assignment
+// iid sampled from a Gamma distribution with alpha=2, beta=1/b
+// C++ ref: https://en.cppreference.com/w/cpp/numeric/random/gamma_distribution
+std::vector<double> generateICSamples(std::vector<double>& ICsamples,
+    const int samplesNo, const double b) {
 
-    // alpha = 2
-    // beta = 1/b
+    std::random_device rd;  // Seed generator
+    std::mt19937 gen(rd());  // Random number generator
+    constexpr double SHAPE_PARAMETER = 2.0; // 'alpha' value as per assignment
+    double SCALE_PARAMETER = 1/b; // 'beta' value as per assignment
+    std::gamma_distribution<> dGamma(SHAPE_PARAMETER, SCALE_PARAMETER);
+
+    for (int i = 1; i<=samplesNo; ++i) {
+        ICsamples.emplace_back(dGamma(gen));
+    }
+
+    return ICsamples;
+}
+
+
+// Cumulative probability of extinction - P(t>s)
+// Calculate P(t>s), according to equation (2) in assignment
+std::vector<double> pExtinctionTimes(std::vector<double>& times, double b) {
+
+    // 'M' value defined in assignment
+    constexpr int IC_SAMPLES = 1e4;
+
+    // Initial conditions vector X0
+    std::vector<double> ICsamples = generateICSamples(ICsamples, IC_SAMPLES, b);
+
+    /* Calculate the extinction time for each IC
+       using the  Euler-Maruyama scheme */
+    std::vector<double> extinctionTimes;
+    std::random_device rd; // Seed generator
+    std::mt19937 gen(rd()); // Random number generator
+    std::normal_distribution<> dStandard(0.0, 1.0); // Standard normal pdf
+    // For each IC (m) in ICsamples (M)
+        // start stepping using the 
+        // if Xn<=0
+            // record the extinction time Tm=n*dt
+            // break out
+    for (int i = 1; i <=IC_SAMPLES; ++i) {
+        // TODO change index to match assignment formulas
+    }
+
+
+    // Calculation of probability
+    std::vector<double> result;
+    // for each time in times
+        // 
+    return result;
 }
 
 
@@ -28,10 +71,8 @@ void serial(double M) {
 
     // Start timer
     // For each b value
-        // Create a gamma(x; b) distribution
-        // Draw M samples from gamma(b), store them in X0(b)
-        // For each time value
-            // Calculate P(t; b,X0)
+        // Create a vector of timestamps
+        // Calculate a vector P(t; b) for these timestamps 
         // Calculate best-fit f(t; lambda) = lambda*exp(-lambda*t)
         // Return lambda(b)
     // Stop timer
