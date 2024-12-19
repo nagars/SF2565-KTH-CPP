1e17a144afda20db35ca04ed852ea7e452ea1a9f
Shawn <shawnnagar@gmail.com>
2024-12-19 16:26:04 +0100

    Added code to run a for loop which runs calculations with a different number of threads each time

--- Assign4/main.cpp
+++ Assign4/main.cpp
@@ -17,11 +17,8 @@ Created on: Dec 6, 2024
 #define DT 				1e-3  	// time step
 #define MAX_B			10		// Max value of b values to use
 #define B_VAL_INCREMENT 1		// b value increment
-#define M				1e4		// M value defined in assignment
-#define NUM_THREADS		4		// Number of threads in parallel implementation
+#define M				5e5		// M value defined in assignment
 
-#define THREADS_ENABLE	1
-#define THREADS_DISABLE	0
 
 void printToFile(const std::vector<std::vector<double>> extinctionTimes,
 		const std::vector<double> b_vals,
@@ -111,7 +108,8 @@ std::vector<double> extinctionTimesSerial(std::vector<double>& times, double b) 
 }
 // Cumulative probability of extinction - P(t>s)
 // Calculate P(t>s), according to equation (2) in assignment
-std::vector<double> extinctionTimesParallel(std::vector<double>& times, double b) {
+std::vector<double> extinctionTimesParallel(
+		std::vector<double>& times, double b, uint16_t numThreads) {
 
 	// Initial conditions vector X0
 	std::vector<double> ICsamples;
@@ -129,7 +127,7 @@ std::vector<double> extinctionTimesParallel(std::vector<double>& times, double b
 	std::vector<std::vector<double>> mextinction;
 
 	// Initialise each thread
-	for (int i = 0; i < NUM_THREADS; ++i) {
+	for (int i = 0; i < numThreads; ++i) {
 
 		// Stores each threads extinction times
 		// Assigned to vector of windows declared above
@@ -137,7 +135,7 @@ std::vector<double> extinctionTimesParallel(std::vector<double>& times, double b
 		mextinction.push_back(windowExtinction);
 
 		taskflow.emplace(
-				[&mextinction, ICsamples, i, b] () {
+				[&mextinction, ICsamples, i, b, numThreads] () {
 			/* Calculate the extinction time for each IC
 				using the  Euler-Maruyama scheme */
 			std::random_device rd; // Seed generator
@@ -145,7 +143,7 @@ std::vector<double> extinctionTimesParallel(std::vector<double>& times, double b
 			std::normal_distribution<> dStandard(0.0, 1.0); // Standard normal pdf
 			//				size_t M = ICsamples.size();
 			// For each IC (m) in ICsamples (M)
-			for (int m = i*M/NUM_THREADS; m < M/NUM_THREADS*(i+1); m++) {
+			for (int m = i*M/numThreads; m < M/numThreads*(i+1); m++) {
 				// start stepping using the EM scheme
 				double Xn = ICsamples[m] ;
 				for (int n = 0; ;n++) {
@@ -180,12 +178,12 @@ std::vector<double> extinctionTimesParallel(std::vector<double>& times, double b
 }
 
 std::vector<double> probabilityExtinctionTimes(std::vector<double>& times,
-		double b, bool enable_threads) {
+		double b, uint16_t numThreads) {
 
 	// Generates vector of extinction times
 	std::vector<double> r_extinctionTimes;
-	if(enable_threads){
-		r_extinctionTimes = extinctionTimesParallel(times, b);
+	if(numThreads > 1){
+		r_extinctionTimes = extinctionTimesParallel(times, b, numThreads);
 	}else{
 		r_extinctionTimes = extinctionTimesSerial(times, b);
 	}
@@ -269,29 +267,19 @@ int main() {
 
 	std::vector<std::vector<double>> extinctionTimes;
 
-	/* ######## TASK 1 ######### */
-	timer.start("Elapsed time 1 Thread: ");
-	// Calculate extinction times in Serial
-	for(size_t n = 0; n < b.size(); n++){
-		extinctionTimes.emplace_back(
-				probabilityExtinctionTimes(timesteps, b[n],
-						THREADS_DISABLE)
-		);
-	}timer.stop();
-
-
-	/* ######## TASK 2 ######### */
-	std::string str("Elapsed time ");
-	str += std::to_string(NUM_THREADS);
-	str += " Threads: ";
-	timer.start(str);
-	// Calculate extinction times in parallel
-	for(size_t n = 0; n < b.size(); n++){
-		extinctionTimes.emplace_back(
-				probabilityExtinctionTimes(timesteps, b[n],
-						THREADS_ENABLE)
-		);
-	}timer.stop();
+	// Calculate for different number of threads
+	std::cout << "Threads | Time Taken (ms): " << std::endl;
+	for(uint16_t threads = 1; threads < 10; threads++){
+		extinctionTimes.clear();
+		timer.start(std::to_string(threads));
+		// Calculate extinction times in parallel
+		for(size_t n = 0; n < b.size(); n++)
+			extinctionTimes.emplace_back(
+					probabilityExtinctionTimes(timesteps, b[n],
+							threads)
+			);
+		timer.stop();
+	}
 
 	printToFile(extinctionTimes, b, timesteps);
 
