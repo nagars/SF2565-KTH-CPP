4a9b5fb78b7f0e89981258f9b9fb1a3ba62cbc49
Shawn <shawnnagar@gmail.com>
2024-12-20 17:06:28 +0100

    Added a number of threads #define to explicitly set number of threads

--- Assign4/main.cpp
+++ Assign4/main.cpp
@@ -17,8 +17,9 @@ Created on: Dec 6, 2024
 #define DT 				1e-3  	// time step
 #define MAX_B			10		// Max value of b values to use
 #define B_VAL_INCREMENT 1		// b value increment
-#define M				1e4		// M value defined in assignment
-#define THREADS_NUM		400		// Number of threads for parallel calc
+#define M				1e5		// M value defined in assignment
+#define THREADS_NUM		20		// Number of threads for parallel calc
+#define WINDOWS_NUM		1000	// Number of windows to divide IC vec into
 
 void printToFile(const std::vector<std::vector<double>> extinctionTimes,
 		const std::vector<double> b_vals,
@@ -116,7 +117,7 @@ std::vector<double> extinctionTimesParallel(
 	generateICSamples(ICsamples, M, b);
 
 	// Taskflow initialisation
-	tf::Executor executor;
+	tf::Executor executor(numThreads);
 	tf::Taskflow taskflow;
 
 	// Vector of vectors of extinction times
@@ -127,7 +128,7 @@ std::vector<double> extinctionTimesParallel(
 	std::vector<std::vector<double>> mextinction;
 
 	// Initialise each thread
-	for (int i = 0; i <= numThreads; ++i) {
+	for (int i = 0; i <= WINDOWS_NUM; ++i) {
 
 		// Stores each threads extinction times
 		// Assigned to vector of windows declared above
@@ -135,15 +136,15 @@ std::vector<double> extinctionTimesParallel(
 		mextinction.push_back(windowExtinction);
 
 		taskflow.emplace(
-				[&mextinction, ICsamples, i, b, numThreads] () {
+				[&mextinction, ICsamples, i, b] () {
 			/* Calculate the extinction time for each IC
-				using the  Euler-Maruyama scheme */
+						using the  Euler-Maruyama scheme */
 			std::random_device rd; // Seed generator
 			std::mt19937 gen(rd()); // Random number generator
 			std::normal_distribution<> dStandard(0.0, 1.0); // Standard normal pdf
 			//				size_t M = ICsamples.size();
 			// For each IC (m) in ICsamples (M)
-			for (int m = i*M/numThreads; m < M/numThreads*(i+1); m++) {
+			for (int m = i*M/WINDOWS_NUM; m < M/WINDOWS_NUM*(i+1); m++) {
 				// start stepping using the EM scheme
 				double Xn = ICsamples[m] ;
 				for (int n = 0; ;n++) {
@@ -165,7 +166,8 @@ std::vector<double> extinctionTimesParallel(
 	}
 
 	// Start threads. Wait for all threads to finish
-	executor.run(taskflow).wait();
+	executor.run(taskflow);
+	executor.wait_for_all();
 
 	// Final vector of extinction times
 	// We combine the vector of windows into a single vector
@@ -175,8 +177,38 @@ std::vector<double> extinctionTimesParallel(
 				mextinction[i].begin(), mextinction[i].end());
 	}
 	return r_extinctionTimes;
-}
 
+	//	std::vector<double> extinctionTimes;
+	//	taskflow.emplace(
+	//			[&extinctionTimes, ICsamples, b] () {
+	//		/* Calculate the extinction time for each IC
+	//	       using the  Euler-Maruyama scheme */
+	//
+	//		std::random_device rd; // Seed generator
+	//		std::mt19937 gen(rd()); // Random number generator
+	//		std::normal_distribution<> dStandard(0.0, 1.0); // Standard normal pdf
+	//		// For each IC (m) in ICsamples (M)
+	//		for (int m = 0; m < M; m++) {
+	//			// start stepping using the
+	//			double Xn = ICsamples[m] ;
+	//			for (int n = 0; ;n++) {
+	//				Xn = Xn + (-b)*DT + sqrt(DT)*dStandard(gen);
+	//				// if Xn<=0
+	//				if (Xn <= 0) {
+	//					// record the extinction time Tm=n*dt
+	//					// break out
+	//					extinctionTimes.emplace_back(n*DT);
+	//					break;
+	//				}
+	//			}
+	//		}
+	//	});
+	//
+	//	executor.run(taskflow);
+	//	executor.wait_for_all();
+	//
+	//	return extinctionTimes;
+}
 std::vector<double> probabilityExtinctionTimes(std::vector<double>& times,
 		double b, uint16_t numThreads) {
 
@@ -203,52 +235,6 @@ std::vector<double> probabilityExtinctionTimes(std::vector<double>& times,
 }
 
 
-//void serial(std::vector<double> timestamps, double M) {
-//	////////////////////////////////////
-//	/* TASK I - Serial implementation */
-//	////////////////////////////////////
-//
-//	// Create a vector of timestamps
-//
-//	// Start timer
-//	// For each b value
-//	// Calculate a vector P(t; b) for these timestamps
-//	// Calculate best-fit f(t; lambda) = lambda*exp(-lambda*t)
-//	// Return lambda(b)
-//	// Stop timer
-//	// Return elapsed time
-
-//	// Generate data for plotting
-//
-//	///// Python code /////
-//	// For each b
-//	// Plot P(t; b) vs t
-//	// Plot best fit f(t)=lambda...
-//}
-
-//void parallel(double M, double b) {
-//	///////////////////////////////////////
-//	/* TASK II - Parallel implementation */
-//	///////////////////////////////////////
-//
-//	// Create a vector of p: number of threads (include 1)
-//	// Create a vector of executionTimes
-//	// for each p value
-//	// Start timer
-//	// Run the same computation as Task I
-//	// Stop timer
-//	// append to executionTime
-//
-//	// calculate speedUp
-//	// Generate data for plotting
-//	// Fit an Amdahl curve to the data
-//
-//	///// Python code /////
-//	// Plot speedUp vs executionTime
-//	// Superimpose Amdahl curve
-//}
-
-
 int main() {
 
 	using namespace std::chrono;
@@ -269,7 +255,7 @@ int main() {
 
 	// Calculate for different number of threads
 	std::cout << "Threads | Time Taken: " << std::endl;
-	for(uint16_t threads = 1; threads < THREADS_NUM; threads++){
+	for(uint16_t threads = 1; threads <= THREADS_NUM; threads++){
 		extinctionTimes.clear();
 		timer.start(std::to_string(threads));
 		// Calculate extinction times in parallel
